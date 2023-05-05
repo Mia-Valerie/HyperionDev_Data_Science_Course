42f90928a3d96bef57724267206e8eff9ab3dfed
Mia Morgan <94843276+Mia-Valerie@users.noreply.github.com>
2023-05-05 14:12:15 +0100

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+name = input("what is your name?")
+print(name)
+age = int(input("What is your age?"))
+print(age)
+print("Hello World!")
---
+++ menu.py
@@ -0,0 +1,6 @@
+item1, item2, item3 = input("Please order your 3 favourite food items on the menu. Press enter after you have typed each item. "), input(), input()
+#I have assigned three varibles in one line, asking once for the 3 items as this is more efficent than asking one at a time, or using a spit function as this only allows for one word food names.#
+print("Order Confirmation! You have ordered:")
+print(item1)
+print(item2)
+print(item3)
